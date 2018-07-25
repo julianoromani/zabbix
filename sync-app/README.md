6cff2a6133238dbf04f05a3edfa1fdf153985cde
Juliano Romani <julianor@gmail.com>
2018-07-25 11:17:58 -0300

    Add support to monitor sync apps
    Add script and template to monitor dropbox or google drive.

---
+++ sync-app/README.md
@@ -0,0 +1,11 @@
+Script para verificar se o processo do dropbox ou do google drive
+está rodando na sessão do usuário especificado.
+Caso não esteja rodando, inicia o processo como o usuário especificado
+e na sessão que está logado.
+
+Ao importar o template, criar o seguinte mapeamento de valor:
+
+Sync App
+0 -> Rodando
+1 -> Reiniciado
+2 -> Erro
---
+++ sync-app/scripts/sync-app.ps1
@@ -0,0 +1,46 @@
+﻿# Procura se o processo do dropbox ou do google drive está em execução
+# na sessão do usuário especificado, caso positivo retorna 0. Se não 
+# estiver rodando, chama o processo na sessão do usuário e retorna 1.
+# Caso o nome do processo não esteja nos padrões, retorna 2.
+# 
+# Author: Juliano Romani
+# Versão: 0.8
+# Data: 2018-07-25
+
+param(
+  [Parameter(Mandatory=$True,Position=0)]
+  [string]$user,
+  [Parameter(Mandatory=$True,Position=1)]
+  [string]$senha,
+  [Parameter(Mandatory=$True,Position=2)]
+  [string]$processo
+)
+
+if ($processo -eq "dropbox"){
+  $processo = "Dropbox"
+}
+elseif ($processo -eq "gdrive"){
+  $processo = "googledrivesync"
+ }
+else {
+  '2'
+}
+
+$existe = (Get-Process -IncludeUserName | Where-Object {$_.ProcessName -match $processo -and $_.UserName -match $user}).Id
+if ($existe ){
+  '0'
+}
+else {
+  $session = (Get-Process -IncludeUserName | Select-Object UserName,SessionId -Unique | Where-Object { $_.UserName -Match $user }).SessionId
+  if ($processo -eq "Dropbox"){
+    & C:\smo\PsExec.exe -i $session -u $user -p $senha -d -accepteula -nobanner 'C:\Program Files (x86)\Dropbox\Client\Dropbox.exe' 2>&1 > $null
+    '1'
+  }
+  elseif ($processo -eq "googledrivesync"){
+    & C:\smo\PsExec.exe -i $session -u $user -p $senha -d -accepteula -nobanner 'C:\Program Files\Google\Drive\googledrivesync.exe' 2>&1 > $null
+    '1'
+  }
+  else {
+    '2'
+  }
+}
